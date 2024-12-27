b4af3cdf53c478d1c989da588666090e619327d5
FoundDream <2979058069@qq.com>
2024-12-27 19:40:43 +0800

    Updates

---
+++ assets/hello-BRZ4jsDx.js
@@ -0,0 +1,25 @@
+const n=`说来惭愧，最早开始学习前端就是为了给自己建网站，期间陆陆续续建了好几个网站，最终都因为各种原因没有坚持下来，不过好在，我用自己最舒服地方式，组织了建立了此网站。
+
+## 关于这个博客
+
+一开始打算使用 Next.js同时构建前后端，但是后来发现，Next.js 的配置太复杂了，会让这个博客很臃肿，我需要一个更简单的方式来构建这个博客。
+
+最终这个博客使用纯前端技术构建：
+
+- React
+- TypeScript
+- Tailwind CSS
+- Vite
+- Markdown
+
+## 未来计划
+
+我计划写一些关于以下主题的文章：
+
+1. 技术向文章
+2. 生活感悟
+3. 工具分享
+
+## 结束
+
+IM SORRY ABOUT THAT DAYS. THIS IS MY FIRST BLOG AND FOR YOU.`;export{n as default};
